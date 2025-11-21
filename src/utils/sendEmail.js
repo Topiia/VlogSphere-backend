26cdbcb43c85ed437de141988b845dcb2533cc82
Topiia <devlop426@gmail.com>
2025-11-21 16:52:36 +0530

    Refactor sendEmail function for improved clarity

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -1,31 +1,25 @@
 const nodemailer = require('nodemailer');
 
 const sendEmail = async (options) => {
-  // Create transporter (fixed createTransport usage)
   const transporter = nodemailer.createTransport({
+    service: process.env.EMAIL_SERVICE,
     host: process.env.EMAIL_HOST,
-    port: Number(process.env.EMAIL_PORT) || 587,
-    secure: process.env.EMAIL_SECURE === 'true' || false, // true for 465, false for other ports
+    port: process.env.EMAIL_PORT,
+    secure: false,
     auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS
+      user: process.env.EMAIL_USERNAME,
+      pass: process.env.EMAIL_PASSWORD
     }
   });
 
-  // Message options
-  const message = {
-    from: `${process.env.FROM_NAME || 'VlogSphere'} <${process.env.FROM_EMAIL || process.env.EMAIL_USER}>`,
-    to: options.to,
+  const mailOptions = {
+    from: process.env.EMAIL_FROM,
+    to: options.email,
     subject: options.subject,
-    text: options.text,
-    html: options.html || undefined
+    html: options.message,
   };
 
-  // Send email
-  const info = await transporter.sendMail(message);
-
-  console.log('Message sent: %s', info.messageId);
-  return info;
+  return await transporter.sendMail(mailOptions);
 };
 
 module.exports = sendEmail;
